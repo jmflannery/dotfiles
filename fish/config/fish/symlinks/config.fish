9e40c8ee389f3fb7a2bef9bb08cc204311ed8a1a
Jack Flannery <jmflannery81@gmail.com>
2017-02-02 17:37:52 -0500

    Update fish and zsh paths

--- fish/config/fish/symlinks/config.fish
+++ fish/config/fish/symlinks/config.fish
@@ -1,5 +1,15 @@
 set -x EDITOR atom
 set -x DOTFILES $HOME/dotfiles
 
+#
 # PATH
-set -x PATH $PATH ~/npm/bin
+#
+set -x PATH $PATH ~/bin ~/npm/bin
+# yarn
+set -x PATH $PATH /usr/local/Cellar/node/7.5.0/bin
+# Postgres.app
+set -x PATH $PATH /Applications/Postgres.app/Contents/Versions/9.3/bin
+
+# chruby
+source /usr/local/share/chruby/chruby.fish
+source /usr/local/share/chruby/auto.fish
